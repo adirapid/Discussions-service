cd3e4af514d2e11fe38188df3697e1037faf8bdc
adirapid <adi.tanami@rapidapi.com>
2017-06-28 09:42:07 +0300

    add association to models

--- models/comments.js
+++ models/comments.js
@@ -1,51 +1,45 @@
+
 module.exports = function (sequelize, DataTypes) {
-  const Comments = sequelize.define("Comments", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    issueId: {
-      type: DataTypes.INTEGER
-    },
-    userId: {
-      type: DataTypes.INTEGER
-    },
-    body: {
-      type: DataTypes.TEXT
-    },
-    top: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    rating: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0
-    },
-    status: {
-      type: DataTypes.TEXT,
-      defaultValue: "active"
-    },
-    createdAt: {
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      type: DataTypes.DATE
+  const Comments = sequelize.define("Comments",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      issueId: {
+        type: DataTypes.INTEGER
+      },
+      userId: {
+        type: DataTypes.INTEGER
+      },
+      body: {
+        type: DataTypes.TEXT
+      },
+      top: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+      status: {
+        type: DataTypes.TEXT,
+        defaultValue: "active"
+      },
+      createdAt: {
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        type: DataTypes.DATE
+      }
     }
-  }
-    // {
-    // classMethods: {
-    //   associate: models => {
-    //     Answers.belongsTo(models.Questions, {
-    //       foreignKey: "questionId"
-    //     });
-    //     Answers.belongsTo(models.Users, {
-    //       foreignKey: "userId"
-    //     });
-    //   }
-    // }
-    // }
   );
+
+  Comments.associate = (models) => {
+    Comments.belongsTo(models.Issues, { foreignKey: "issueId" });
+  };
 
   return Comments;
 };
--- models/issues.js
+++ models/issues.js
@@ -1,52 +1,48 @@
 
 module.exports = function (sequelize, DataTypes) {
-  const Issues = sequelize.define("Issues", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    userId: {
-      type: DataTypes.INTEGER
-    },
-    topicId: {
-      type: DataTypes.INTEGER
-    },
-    title: {
-      type: DataTypes.TEXT
-    },
-    body: {
-      type: DataTypes.TEXT
-    },
-    rating: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0
-    },
-    commentsCount: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0
-    },
-    status: {
-      type: DataTypes.TEXT,
-      defaultValue: "active"
-    },
-    createdAt: {
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      type: DataTypes.DATE
-    }
-  },
-  {
-    classMethods: {
-      associate: (models) => {
-        Issues.hasMany(models.Comments, {
-          foreignKey: "issueId"
-        });
+  const Issues = sequelize.define("Issues",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      userId: {
+        type: DataTypes.INTEGER
+      },
+      topicId: {
+        type: DataTypes.INTEGER
+      },
+      title: {
+        type: DataTypes.TEXT
+      },
+      body: {
+        type: DataTypes.TEXT
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+      commentsCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+      status: {
+        type: DataTypes.TEXT,
+        defaultValue: "active"
+      },
+      createdAt: {
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        type: DataTypes.DATE
       }
     }
-  }
   );
+
+  Issues.associate = (models) => {
+    Issues.hasMany(models.Comments, { foreignKey: "issueId" });
+  };
 
   return Issues;
 };
